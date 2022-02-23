a3483f50631fad94625b8d58d9bb44a899fae0fd
Osipkov K <kirill25osipkov@gmail.com>
2022-02-23 21:45:00 +0200

    готов material click, остались анимации кнопок и выпадающее меню

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ var elements = {};
 function findElements() {
     elements['main_btn'] = $('.btn-main');
     elements['nav_btn'] = $('.navigation__button');
+    elements['main_menu_elem'] = $('.main-menu__item');
 }
 
 function ButtonMouseIn(){
@@ -15,10 +16,31 @@ function ButtonMouseOut(){
     $(this).addClass("btn-main--mouse-out");
 }
 
+function materialEffect(e) {
+    const wave = document.createElement('span'),
+    waveOffset = this.getBoundingClientRect();
+    $(wave).addClass('main-menu__material-temp-element')
+
+    const waveY = e.pageY - waveOffset.top,
+    waveX = e.pageX - waveOffset.left;
+
+    wave.style.top = waveY + 'px',
+    wave.style.left = waveX + 'px',
+
+
+    this.appendChild(wave);
+
+    setTimeout(function () {
+        wave.parentNode.removeChild(wave);
+        }, 1500);
+}
 
 function setAction() {
     $(elements.main_btn).on('mouseenter', ButtonMouseIn);
     $(elements.main_btn).on('mouseleave', ButtonMouseOut);
+    $(elements.main_menu_elem).on('click', materialEffect);
+
+
 
 }
 
